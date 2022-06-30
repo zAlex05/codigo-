1e5cd472c2ff758c052b0540891be97ce53bdd48
zAlex05 <107196854+zAlex05@users.noreply.github.com>
2022-06-30 12:47:40 -0500

    Create Comentarios.php

---
+++ Comentarios.php
@@ -0,0 +1,17 @@
+<?php 
+require_once("Conexion.php");
+class Comentarios  extends Conexion
+{
+	
+	function __construct(argument)
+	{
+		// code...
+	}
+
+	public function listar_comentarios(){
+		
+	}
+
+}
+
+?>
